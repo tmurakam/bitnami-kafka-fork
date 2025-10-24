942f13937272f528d05a7faf2b4a46366ea5cdf2
Takuya Murakami <murakami_da@nec.com>
2025-10-24 20:30:23 +0900

    fix: patch scripts for 3.9

--- 3.9/prebuildfs/opt/bitnami/scripts/libbitnami.sh
+++ 3.9/prebuildfs/opt/bitnami/scripts/libbitnami.sh
@@ -47,7 +47,7 @@ print_image_welcome_page() {
     info ""
     info "${BOLD}Welcome to the Bitnami ${BITNAMI_APP_NAME} container${RESET}"
     info "Subscribe to project updates by watching ${BOLD}${github_url}${RESET}"
-    info "Did you know there are enterprise versions of the Bitnami catalog? For enhanced secure software supply chain features, unlimited pulls from Docker, LTS support, or application customization, see Bitnami Premium or Tanzu Application Catalog. See https://www.arrow.com/globalecs/na/vendors/bitnami/ for more information."
+    info "${YELLOW}NOTICE: This is a forked version of bitnami image and not official.${RESET}"
     info ""
 }
 
